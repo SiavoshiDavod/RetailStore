b28bc3b9a9cb6ba3b5b39a082385325b15006627
SiavoshiDavod <siavoshi.davod@gmail.com>
2025-11-22 17:43:26 +0330

    add8

---
+++ src/services/AuthService/AuthService.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace AuthService.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMapster();
+
+        return services;
+    }
+}
